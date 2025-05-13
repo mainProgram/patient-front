cd752730697bd5da7fed15c115c8191d20fe12d8
devByJESUS <mmnledgar@gmail.com>
2025-05-13 20:10:31 +0000

    optimize selenium script

--- tests/auth.py
+++ tests/auth.py
@@ -3,6 +3,8 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
@@ -15,24 +17,27 @@ import time
 
 # Initialiser le navigateur
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-
+wait = WebDriverWait(driver, 15)
 try:
     # Aller à la page du formulaire de login Angular
     driver.get("http://localhost:4200/")
     time.sleep(5)  # Attendre le chargement
 
     # Trouver les champs
-    username_input = driver.find_element(By.NAME, "username")
+    #username_input = driver.find_element(By.NAME, "username")
+    username_input = wait.until(EC.element_to_be_clickable((By.NAME, "username")))
     time.sleep(5)
-    password_input = driver.find_element(By.NAME, "password")
+    #password_input = driver.find_element(By.NAME, "password")
+    password_input = wait.until(EC.element_to_be_clickable((By.NAME, "password")))
     time.sleep(5)
-    login_button = driver.find_element(By.TAG_NAME, "button")
+    #login_button = driver.find_element(By.TAG_NAME, "button")
+    submit_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 
 
     # Entrer les données
     username_input.send_keys("admin")
     password_input.send_keys("admin123")
-    login_button.click()
+    submit_button.click()
 
     # Attente du résultat
     time.sleep(10)
